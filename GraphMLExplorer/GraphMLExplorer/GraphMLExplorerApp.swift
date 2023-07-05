ce4c63788c3d50129d6eeb90d4a8c6b04abcc850
Bartłomiej Zdybowicz <bartlomiej.zdybowicz@gmail.com>
2023-07-05 12:24:32 +0200

    create project

---
+++ GraphMLExplorer/GraphMLExplorer/GraphMLExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphMLExplorerApp.swift
+//  GraphMLExplorer
+//
+//  Created by Bartlomiej Zdybowicz on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GraphMLExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
