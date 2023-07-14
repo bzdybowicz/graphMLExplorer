099d19d4f6cdeef0c83af6d7968020de10da4dde
Bartłomiej Zdybowicz <bartlomiej.zdybowicz@gmail.com>
2023-07-14 14:19:22 +0200

    add simple snapshot to get to know when sth changes

---
+++ GraphMLExplorer/GraphMLExplorerTests/GraphViewSnapshotsTests.swift
@@ -0,0 +1,26 @@
+//
+//  GraphViewSnapshotsTests.swift
+//  GraphMLExplorerTests
+//
+//  Created by Bartlomiej Zdybowicz on 14/07/2023.
+//
+
+import Foundation
+@testable import GraphMLExplorer
+import SnapshotTesting
+import XCTest
+
+final class GraphViewSnapshotsTests: XCTestCase {
+
+    func test() {
+        let loader = UnweightedGraphLoader()
+        let graph = loader.start()
+        let sut = GraphView(graph: graph, unweightedGraphLoader: loader)
+#if os(iOS)
+        assertSnapshot(matching: sut.frame(width: 1000, height: 600), as: .image, named: "iphone600x600start")
+#elseif os(macOS)
+
+#endif
+    }
+
+}
